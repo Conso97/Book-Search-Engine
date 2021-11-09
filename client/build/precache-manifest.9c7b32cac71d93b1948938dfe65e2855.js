10468b26867b67aa55be03dbe5c8cdc98a791817
Consolata Njeri <conso4u@gmail.com>
2021-11-08 23:42:56 -0800

    debug by adding build directory

---
+++ client/build/precache-manifest.9c7b32cac71d93b1948938dfe65e2855.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d98e7eafbdd5a3b45effaf16c14c30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baddda4df4eb36e5ca15",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "de16752f220934978d65",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "baddda4df4eb36e5ca15",
+    "url": "/static/js/2.46f41d5b.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.46f41d5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de16752f220934978d65",
+    "url": "/static/js/main.947c0375.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
