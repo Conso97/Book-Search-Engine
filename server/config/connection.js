d38f584b651be34b45556e972e4dfa35bd93febf
Consolata Njeri <conso4u@gmail.com>
2021-11-09 19:09:07 -0800

    finished env variable test

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://conso_97:Malaika911@cluster0.fv1gm.mongodb.net/book?retryWrites=true&w=majority" || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
