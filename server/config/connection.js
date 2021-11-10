d0e2b2ece784d54babd8a710d5700907791b2e8a
Consolata Njeri <conso4u@gmail.com>
2021-11-09 19:04:57 -0800

    debug with env vars

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect("mongodb+srv://conso_97:Malaika911@cluster0.fv1gm.mongodb.net/book?retryWrites=true&w=majority" || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
